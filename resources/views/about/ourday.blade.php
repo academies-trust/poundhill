0e4be88fba3087165665b62e66b592d88b937cc3
Sian <s.ellis@hastingsacademiestrust.org.uk>
2015-08-10 13:55:27 +0000

    Our Academy Day revamped

--- resources/views/about/ourday.blade.php
+++ resources/views/about/ourday.blade.php
@@ -21,13 +21,27 @@
 	</div>
 	<div class="row">
 		<div class="col-md-12">
-			<p>The Academy day begins promptly at 8.50 am and ends at 2.50 pm.</p>
-			<p>The doors will open at:<br/>
-			<span class="h3">Early Years  8.30 am</span><br/>
-			<span class="h3">Key Stage One  8.40 am</span></p>
+			<p class="lead">The Academy day begins promptly at 8.50 am and ends at 2.50 pm.</p>
+			<p class="text-maroon"><strong>The doors will open at:</strong><br/>
+			<dl>
+				<dt>Early Years</dt>
+				<dd>08:30am</dd>
+				<dt>Key Stage One</dt>
+				<dd>08:40am</dd>
+			</dl>
+			
 			<p>The doors will close at 8.45 am and all children must be in their classroom by 8.50 am ready to start learning.</p>
-			<p>Before the start of the Academy day, Foundation Stage parents should wait on the playground outside Apple, Elm and Ash classrooms. Year One and Two parents should wait on the main playground.</p>
-			<p>In order to encourage independence we kindly ask that children in Year 1 and 2 enter the classrooms on their own.  The emphasis will be on the children organising themselves for the Academy day but should they need help there will be staff available to guide them.  If you need to see the class teacher please enter via the classroom door but be mindful that their time will be limited.  They will however be happy for you to make an appointment at the end of the day should you need to. If someone other than yourself is collecting your child at the end of the Academy day please ensure either the teacher or the office are kept informed.</p>
+			<p><span class="text-maroon"><strong>Before the start of the Academy day,</strong></span><br /> <u>Foundation Stage</u> parents should wait on the playground outside Apple, Elm and Ash classrooms.<br /> <u>Year One and Two</u> parents should wait on the main playground.</p>
+			<figure class="spacer"><img src='{{url()}}/img/generic/ph-27-03-14-030.jpg?w=1920&h=500&fit=crop'></figure>
+			<div class="clear"></div>
+			<p class="spacer"><strong>In order to encourage independence we kindly ask that children in Year 1 and 2 enter the classrooms on their own.</strong> The emphasis will be on the children organising themselves for the Academy day but should they need help there will be staff available to guide them.</p>
+			<div class="bg-info padding-x2">
+				<p><span class="glyphicon glyphicon-info-sign"></span> If you need to see the class teacher please enter via the classroom door but be mindful that their time will be limited.  They will however be happy for you to make an appointment at the end of the day should you need to.</p>
+			</div>
+			<div class="bg-warning padding-x2 spacer">
+				<p><span class="glyphicon glyphicon-info-sign"></span> If someone other than yourself is collecting your child at the end of the Academy day, please ensure either the teacher or the office are kept informed.</p>
+			</div>
+
 		</div>
 	</div>
 	
